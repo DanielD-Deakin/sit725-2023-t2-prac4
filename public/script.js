f4715fa4bd748d0d390ee17f314990a537232cff
Daniel D <s222311422@deakin.edu.au>
2023-08-05 18:24:01 +1000

    Fixed card creation and MongoDB entry storage

--- public/script.js
+++ public/script.js
@@ -62,37 +62,55 @@ const clickMe = () => {
 const addCards = (items) => {
     items.forEach(item => {
         let itemToAppend = '<div class="col s4 center-align">'+
-        '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+        '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
         '</div><div class="card-content">'+
-        '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+        '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.path+'</a></p></div>'+
         '<div class="card-reveal">'+
-        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-        '<p class="card-text">'+item.desciption+'</p>'+
+        '<span class="card-title grey-text text-darken-4">'+item.subTitle+'<i class="material-icons right">close</i></span>'+
+        '<p class="card-text">'+item.description+'</p>'+
         '</div></div></div>';
         $("#card-section").append(itemToAppend)
     });
 }
-    
 
 $(document).ready(function(){
     $('.materialboxed').materialbox();
-    $('#clickMeButton').click(()=>{
-        clickMe();
-    })
-    addCards(cardList);
-    $('.modal').modal();
-    $('#formSubmit').on('click', function(){
+    $('#formSubmit').click(() =>{
         submitForm();
-    });
+    })
+    $('.modal').modal();
+    getAllScenery();
 });
     
 
 const submitForm = () => {
     let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
+    formData.title = $('#title').val();
+    formData.subTitle = $('#subTitle').val();
+    formData.path = $('#path').val();
+    formData.description = $('#description').val();
     console.log("Form Data Submitted: ", formData);
+    postScenery(formData);
 }
-    
+
+function postScenery(scenery) {
+    $.ajax({
+        url: '/api/scenery',
+        type: 'POST',
+        data: JSON.stringify(scenery),
+        contentType: 'application/json', 
+        success: (result) => {
+            if (result.statusCode === 201) {
+                alert('Scenery post successful');
+            }
+        }
+    });
+}
+
+function getAllScenery() {
+    $.get('/api/sceneries', (response) => { 
+        if (response.data && response.data.length > 0) {
+            addCards(response.data);
+        }
+    })
+}
--- server.js
+++ server.js
@@ -2,6 +2,30 @@ var express = require("express");
 var app = express();
 var path = require("path");
 var port = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://s222311422:fM95CKO1wWNBqmAt@cluster0.mampmog.mongodb.net/?retryWrites=true&w=majority";
+let collection;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function runDBConnection() {
+    try {
+        await client.connect();
+        collection = client.db().collection('Scenery');
+        console.log(collection);
+    } catch(ex) {
+        console.error(ex);
+    }
+}
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "images")));
@@ -9,4 +33,36 @@ app.use("/fonts", express.static(path.join(__dirname, "fonts")));
 
 app.listen(port, () => {
     console.log("App listening to: " + port);
+    runDBConnection();
 });
+
+app.get('/api/sceneries', (req, res) => {
+    getAllScenery((err, result)=> {
+        if (!err) {
+            res.json({statusCode: 20, data:result, message:'Get All Scenery Successful'});
+        }
+    });
+});
+
+app.post('/api/scenery', (req, res)=> {
+    let scenery = req.body;
+    postScenery(scenery, (err, result) => {
+        if (!err) {
+            res.json({statusCode: 201, data:result, message:'Success'});
+        }
+    });
+});
+
+function postScenery(scenery, callback) {
+    collection.insertOne(scenery, callback);
+}
+
+function getAllScenery(scenery, callback) {
+    collection.find({}).toArray(scenery, callback);
+}
+
+
+
+
+
+
